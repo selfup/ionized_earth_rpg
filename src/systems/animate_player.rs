3d58046cf23b46c19c7d6217d00e510eef70d82a
selfup <boudinot.regis@yahoo.com>
2022-02-15 21:32:20 -0600

    refactor animate player system

--- src/systems/animate_player.rs
+++ src/systems/animate_player.rs
@@ -2,6 +2,16 @@ use bevy::prelude::*;
 
 use crate::entities::*;
 
+/*
+    [
+        0,1,2,3,4, <- idle
+        5,6,7,8,9, <- left
+        10,11,12,13,14, <- right
+        15,16,17,18,19 <- up
+        20,21,22,23,24, <- down
+    ]
+*/
+
 pub fn animate_player(
     time: Res<Time>,
     mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &mut Player)>,
@@ -9,50 +19,28 @@ pub fn animate_player(
     for (mut timer, mut sprite, mut player) in query.iter_mut() {
         timer.tick(time.delta());
 
-        /*
-            [
-                0,1,2,3,4, <- idle
-                5,6,7,8,9, <- left
-                10,11,12,13,14, <- right
-                15,16,17,18,19 <- up
-                20,21,22,23,24, <- down
-            ]
-        */
-
         if player.idle {
-            if player.animation_index < 4 {
-                player.animation_index += 1;
-            } else {
-                player.animation_index = 0;
-            }
+            animation_index_bounds_calc(&mut player, 4, 0);
         } else if player.left {
-            if player.animation_index < 9 {
-                player.animation_index += 1;
-            } else {
-                player.animation_index = 5;
-            }
+            animation_index_bounds_calc(&mut player, 9, 5);
         } else if player.right {
-            if player.animation_index < 14 {
-                player.animation_index += 1;
-            } else {
-                player.animation_index = 10;
-            }
+            animation_index_bounds_calc(&mut player, 14, 10);
         } else if player.up {
-            if player.animation_index < 19 {
-                player.animation_index += 1;
-            } else {
-                player.animation_index = 15;
-            }
+            animation_index_bounds_calc(&mut player, 19, 15);
         } else if player.down {
-            if player.animation_index < 24 {
-                player.animation_index += 1;
-            } else {
-                player.animation_index = 20;
-            }
+            animation_index_bounds_calc(&mut player, 24, 20);
         }
 
         if timer.finished() {
             sprite.index = player.animation_index as usize;
         }
+    }
+}
+
+fn animation_index_bounds_calc(player: &mut Player, upper_bound: u32, lower_bound: u32) {
+    if player.animation_index < upper_bound {
+        player.animation_index += 1;
+    } else {
+        player.animation_index = lower_bound;
     }
 }
