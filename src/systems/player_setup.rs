4cf3211eb14b324f363eb0128ff29b8783e1c46e
Regis Boudinot <selfup@users.noreply.github.com>
2025-02-15 14:44:25 -0600

    fix player setup code breaks for now - work on values later

--- src/systems/player_setup.rs
+++ src/systems/player_setup.rs
@@ -4,24 +4,41 @@ use crate::entities::*;
 
 const PLAYER_SPRITE_SHEET: &str = "player.png";
 
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
 pub fn player_setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let texture_handle = asset_server.load(PLAYER_SPRITE_SHEET);
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 5, 5);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
     let player = Player::new();
 
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    let animation_indices = AnimationIndices { first: 1, last: 6 };
+
+    let texture = asset_server.load(PLAYER_SPRITE_SHEET);
+    let texture_atlas = TextureAtlasLayout::from_grid(UVec2::splat(16), 5, 5, None, None);
+    let texture_atlas_layout = texture_atlases.add(texture_atlas);
+
+    commands.spawn(Camera2d);
     commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(1.0)),
-            ..Default::default()
-        })
-        .insert(Timer::from_seconds(0.1, true))
+        .spawn((
+            Sprite::from_atlas_image(
+                texture,
+                TextureAtlas {
+                    layout: texture_atlas_layout,
+                    index: animation_indices.first,
+                },
+            ),
+            Transform::from_scale(Vec3::splat(6.0)),
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        ))
         .insert(player);
 }
